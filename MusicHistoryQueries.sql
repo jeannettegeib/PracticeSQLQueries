408e5a192ba313155a816b4bf5b5e477b7feb346
Jeannette Geib <jeannettegeib@gmail.com>
2022-10-11 06:13:33 -0400

    SQL Practice series-- Music History and Poems By Kids

---
+++ MusicHistoryQueries.sql
@@ -0,0 +1,56 @@
+﻿--Write a SELECT query that lists all the Artists that have a Jazz or Rock Album
+	--SELECT artist.ArtistName, genre.Label FROM Artist 
+	--LEFT JOIN Album on album.ArtistId =artist.id
+	--LEFT JOIN Genre on genre.id = album.GenreId
+	--WHERE genre.Label = 'Jazz' OR  genre.Label ='Rock'
+	--ORDER BY genre.Label;
+
+--Write a SELECT statement that lists the Albums with no songs
+--SELECT album.Title FROM Album
+--LEFT JOIN Song on album.Id  = song.AlbumId
+--WHERE song.AlbumId IS Null;
+
+--10
+--SELECT artist.ArtistName, album.Title, song.Title FROM Artist
+--LEFT JOIN Album on album.ArtistId = artist.Id
+--LEFT Join Song on song.AlbumId = album.Id
+--WHERE album.Title = 'The Black Album';
+
+--11
+--Select * FROM song;
+
+--SELECT album.title, COUNT(song.title) as '# of Songs' FROM Album 
+--LEFT JOIN Song on album.id = song.AlbumId
+--Group by album.Title;
+
+--12
+--Select artist.ArtistName, Count(song.Title) AS '#ofSongs' FROM Artist
+--LEFT JOIN Song on artist.Id = song.ArtistId
+--Group by artist.ArtistName
+
+--Select ArtistId from Song Group By ArtistId
+
+--13
+--SELECT genre.[label], Count(song.Title) as '# of Songs' FROM Genre
+--LEFT JOIN Song on genre.id = song.GenreId
+--Group By genre.[label]
+
+--14
+--?????
+--SELECT Artist.Id, Artist.ArtistName, COUNT(Album.id) AS '# Of Labels' FROM Artist
+--LEFT JOIN Album on artist.Id = album.ArtistId
+--Group by album.[Label]
+--Having COUNT (DISTINCT Album.id) >1; 
+
+--15
+--Select Title, MAX(AlbumLength) FROM Album;
+
+
+--16
+Select TOP 1 SongLength, Title from Song
+Order By SongLength DESC;
+
+--17
+Select TOP 1 song.SongLength, song.Title, album.Title from Song
+LEFT JOIN Album on song.AlbumId = album.id
+Order By song.SongLength DESC;
---
+++ PoemsByKidsQueries.sql
@@ -0,0 +1,85 @@
+﻿--4-6
+--select top 76 author.[name], grade.[name], gender.[name] FROM Author 
+--LEFT JOIN grade on author.GradeId = grade.Id
+--LEFT JOIN gender on author.GenderId = gender.Id
+--ORDER BY Author.Name;
+
+--7
+--select SUM(WordCount) From poem;
+
+--8
+--SELECT title, charcount from Poem 
+--Where CharCount = (SELECT TOP 1 charcount from poem Order by CharCount ASC);
+
+--9,10
+--Select Count(author.id) FROM Author
+--LEFT JOIN grade on author.GradeID = grade.id
+--WHERE grade.name = '3rd Grade' OR grade.name = '2nd Grade' OR grade.name = '1st Grade';
+
+--11
+--SELECT COUNT(Poem.Id) FROM Poem 
+--LEFT JOIN author on poem.AuthorId = author.id
+--LEFT JOIN grade on author.GradeId = grade.id
+--WHERE grade.Name='4th Grade';
+
+--12
+--SELECT COUNT(Poem.Id), grade.name FROM Poem 
+--LEFT JOIN author on poem.AuthorId = author.id
+--LEFT JOIN grade on author.GradeId = grade.id
+--GROUP BY grade.name;
+
+--13
+--SELECT COUNT(author.id), grade.name FROM Author
+--LEFT JOIN grade on author.GradeId = grade.id
+--GROUP BY grade.name
+--ORDER BY grade.name;
+
+--14
+--SELECT TOP 1 poem.Title from POEM ORDER BY WordCount DESC;
+
+--15 ???????????????
+--SELECT author.id, COUNT(poem.AuthorID) as '# Of Poems' from Author
+--LEFT JOIN Poem on Author.Id = Poem.AuthorId
+--GROUP BY author.id 
+--ORDER BY Count(poem.AuthorID) DESC;
+
+--16
+--SELECT Count(poem.id) FROM POEM 
+--LEFT JOIN PoemEmotion on poem.Id = PoemEmotion.PoemId
+--LEFT JOIN Emotion on Emotion.Id = PoemEmotion.EmotionId
+--WHERE Emotion.[Name] = 'Sadness';
+
+--17
+--SELECT Count(poem.id) FROM POEM 
+--LEFT JOIN PoemEmotion on poem.Id = PoemEmotion.PoemId
+--WHERE PoemEmotion.id Is Null;
+
+
+--18
+
+--SELECT TOP 1 emotion.Name, COUNT(poem.id) as '#OfPoems' FROM POEM 
+--LEFT JOIN PoemEmotion on poem.Id = PoemEmotion.PoemId
+--LEFT JOIN Emotion on Emotion.Id = PoemEmotion.EmotionId
+--WHERE Emotion.Name IS NOT NULL
+--GROUP by emotion.name
+--ORDER by COUNT(poem.id);
+
+----19
+--SELECT  TOP 1 grade.Name, COUNT(poem.id) as '#OfPoems' FROM POEM 
+--LEFT JOIN author on poem.AuthorID = Author.ID
+--LEFT JOIN grade on Author.GradeId = grade.ID
+--LEFT JOIN PoemEmotion on poem.Id = PoemEmotion.PoemId
+--LEFT JOIN Emotion on Emotion.Id = PoemEmotion.EmotionId
+--WHERE Emotion.Name = 'Joy'
+--Group by grade.name
+--ORDER by COUNT(poem.id) DESC;
+
+----20
+--SELECT TOP 1  gender.Name, COUNT(poem.id) as '#OfPoems' FROM POEM 
+--LEFT JOIN author on poem.AuthorID = Author.ID
+--LEFT JOIN gender on Author.GenderId = gender.ID
+--LEFT JOIN PoemEmotion on poem.Id = PoemEmotion.PoemId
+--LEFT JOIN Emotion on Emotion.Id = PoemEmotion.EmotionId
+--WHERE Emotion.Name = 'Fear'
+--Group by gender.name
+--ORDER by COUNT(poem.id);
